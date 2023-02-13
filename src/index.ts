d51269f70233994c8328bd3901277933f98e5914
luowenhao8663 <869213720@qq.com>
2023-02-13 17:13:24 +0800

    🎉 init: 初始化项目

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+export { arrayEqual } from './arrayEqual';
+export { getCookie, setCookie, removeCookie } from './cookie';
+export { getBrowserInfo } from './device';
+export { formatFloat } from './formatFloat';
+export { formatMoney } from './formatMoney';
+export { convertImgStreamToBase64, downloadImgStream } from './img';
+export { randomColor, randomNum, randomPhone, randomName } from './random';
+export { isLeapYear, remainTime, isSameDay, toTime } from './date';
+export { deepClone } from './deepClone';
+export { sum } from './sum';
+export { parseQueryString, stringfyQueryString } from './urlQuery';
+export {
+  isColor,
+  isEmail,
+  isIdCard,
+  isPhoneNum,
+  isUrl,
+  isMobile,
+  isAppleDevice,
+  isObject,
+  isFunction,
+  isString,
+  isBoolean,
+  isNumber,
+  isUndef,
+} from './verify';
